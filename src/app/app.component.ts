4fa35f34089bfea6b4b95a192a3d0a45dd05a330
Othmane Abderrazik <othmane232004@gmail.com>
2025-01-07 19:58:50 +0100

    api - roles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from './roles/roles.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { MasterComponent } from './components/master/master.component';
+import { DesignationComponent } from './components/designation/designation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RolesComponent],
+  imports: [RouterOutlet, RolesComponent, DesignationComponent, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
