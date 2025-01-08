eb74cdca7cf5de54ac55eebdb93bdcf470572fd5
Othmane Abderrazik <othmane232004@gmail.com>
2025-01-08 01:28:35 +0100

    services

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-designation',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit{
+
+  designationList : IDesignation[] = [];
+  masterService = inject(MasterService);
+
+  ngOnInit():void {
+    this.masterService.getDesignations().subscribe((res:APIResponseModel) => {
+      this.designationList = res.data;
+    },error=>{
+      alert("API error / Network Down")
+    });
+  }
+
 
 }
